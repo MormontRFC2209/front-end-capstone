3ef0f460fed83356076528678fc7665060dc64e3
xincheng zhou <prifvibea@gmail.com>
2022-10-13 17:30:55 -0500

    xincheng 10/13

--- client/src/components/R&R/R&R.jsx
+++ client/src/components/R&R/R&R.jsx
@@ -1,10 +1,23 @@
 import {useState, useEffect} from "react";
+import axios from 'axios';
+import ReviewList from "./reviewList/reviewList.jsx";
 
 
+export default function RANDR(props) {
+  const [reviews,setReviews]=useState([]);
 
 
-export default function RANDR() {
+  useEffect(() => {
+    axios.get("/info", {params: {route: '/reviews/', apiParams: {product_id:66643}}})
+      .then((response) => {
+        setReviews(response.data.results);
+      })
+  }, []);
+
   return (
-    <div> R and R </div>
-  );
+    <>
+    <h5>RATINGS  REVIEWS</h5>
+    <ReviewList reviews={reviews}/>
+    </>
+  )
 }
