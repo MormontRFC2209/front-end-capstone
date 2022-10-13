e4fe4884d20ededf98b28b3f6b1954ff9d5fb63e
Caleb <calebdunderwood@gmail.com>
2022-10-13 00:07:11 -0500

    Server now handles Get and Post requests consistently

---
+++ server/controllers.js
@@ -0,0 +1,67 @@
+const configs = require('../config.js');
+const axios = require('axios');
+
+
+module.exports.makeGETAPICall = (apiObject, callback) => {
+
+  var route = configs.apiServer + apiObject[0].route
+
+  if(apiObject[1]) {
+    console.log(apiObject[1])
+
+    var kvPair = apiObject[1].params
+    axios.get(route, {headers: {Authorization: configs.token}, params: kvPair} )
+    .then((result) => {
+      callback(result.data, null)
+    })
+    .catch((error) => {
+      callback(null, error)
+    })
+
+  } else {
+
+    axios.get(route, {headers: {Authorization: configs.token}})
+      .then((result) => {
+        callback(result.data, null)
+      })
+      .catch((error) => {
+        callback(null, error)
+      })
+  }
+
+
+}
+
+module.exports.makePOSTAPICall = (apiObject, callback) => {
+
+  var route = configs.apiServer + apiObject[0].route
+  var bodyObject = apiObject[1].body
+
+  if(apiObject[2]) {
+
+    var paramsObject = apiObject[2].params
+
+    axios.post(route, kvPair ,{headers: {Authorization: configs.token}, params: paramsObject})
+      .then((result) => {
+        callback(result.data, null)
+      })
+      .catch((error) => {
+        callback(null, error)
+      })
+
+
+  } else {
+
+    axios.post(route, bodyObject ,{headers: {Authorization: configs.token}})
+    .then((result) => {
+      callback(result.data, null)
+    })
+    .catch((error) => {
+      callback(null, error)
+    })
+
+  }
+
+
+
+}
