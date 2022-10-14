bf728eb3b46ce98a980d513122938ede93a0319f
Sarah Handley <handleysarah99@gmail.com>
2022-10-14 17:20:55 -0500

    add price to product heading

--- client/src/components/Overview/ProductHeading/ProductHeading.jsx
+++ client/src/components/Overview/ProductHeading/ProductHeading.jsx
@@ -1,10 +1,19 @@
 import react from 'react';
 
-export default function ProductHeading({ productInfo }) {
+export default function ProductHeading({ productInfo, styles, selectedStyleId }) {
   return (
     <div>
       <h4>{productInfo[0].category}</h4>
       <h2>{productInfo[0].name}</h2>
+      {styles[selectedStyleId].sale_price === null &&
+        <div>${styles[selectedStyleId].original_price}</div>
+      }
+      {styles[selectedStyleId].sale_price !== null &&
+        <div>
+          <div style={{color: 'red'}}>${styles[selectedStyleId].sale_price}</div>
+          <div style={{textDecoration: 'line-through'}}>${styles[selectedStyleId].original_price}</div>
+        </div>
+      }
     </div>
   );
 };
