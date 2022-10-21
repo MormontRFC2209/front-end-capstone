4d9a7fbf7ccb8a8b4a9c8051a3dc974f13d09e96
Sarah Handley <handleysarah99@gmail.com>
2022-10-21 12:58:52 -0500

    work on starting tests

--- client/__tests__/app.test.js
+++ client/__tests__/app.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
-import ProductHeading from '../src/components/Overview/ProductHeading/ProductHeading.jsx';
+import App from '../src/components/App.jsx';
 
 describe('Overview Tests', function() {
   const user = userEvent.setup();
 
   it('should display product name', function() {
-    const productName = screen.getByType('h4');
+    render(<App />);
+    const productName = screen.getByTestId('product-name');
     expect(productName).toEqual('Heir Force Ones');
   });
 });
