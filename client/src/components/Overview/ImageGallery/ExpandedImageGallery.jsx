d0264a64f6ee9fb3b5c5a2605d83536a7d7f8904
Sarah Handley <handleysarah99@gmail.com>
2022-10-21 22:26:52 -0500

    add zoom functionality

--- client/src/components/Overview/ImageGallery/ExpandedImageGallery.jsx
+++ client/src/components/Overview/ImageGallery/ExpandedImageGallery.jsx
@@ -4,6 +4,31 @@ import ForwardArrow from './ForwardArrow.jsx';
 
 export default function ExpandedImageGallery({ photos, photoId, expanded, setExpanded, setPhotoId, handleBackArrowClick, handleForwardArrowClick }) {
   const [zoom, setZoom] = useState(false);
+  const [expandedDimensions, setExpandedDimensions] = useState([0, 0]);
+  const [zoomSize, setZoomSize] = useState('');
+
+  const handleZoomClick = (e) => {
+    setZoom(true);
+    setExpandedDimensions([e.currentTarget.clientWidth, e.currentTarget.clientHeight]);
+    if (e.currentTarget.clientWidth >= e.currentTarget.clientHeight) {
+      setZoomSize(`${e.currentTarget.clientWidth * 2.5}px auto`);
+    } else {
+      setZoomSize(`auto ${e.currentTarget.clientHeight * 2.5}px`);
+    }
+  };
+
+  const zoomIn = (e) => {
+    const zoomTarget = e.currentTarget;
+    let clientX = 0;
+    let clientY = 0;
+    e.clientX ? clientX = e.clientX : clientX = e.pageX;
+    e.clientY ? clientY = e.clientY : clientX = e.pageX;
+    let offsetX = clientX + zoomTarget.offsetWidth - zoomTarget.clientWidth;
+    let offsetY = clientY + zoomTarget.offsetWidth - zoomTarget.clientWidth;
+    let x = offsetX/zoomTarget.offsetWidth*100;
+    let y = offsetY/zoomTarget.offsetHeight*100;
+    zoomTarget.style.backgroundPosition = x + '%' + y + '%';
+  };
 
   return (
     <div className='expanded-image-gallery'>
@@ -11,7 +36,17 @@ export default function ExpandedImageGallery({ photos, photoId, expanded, setExp
         <div className='close-expanded' onClick={() => setExpanded(false)}>
           <i className="fa-solid fa-xmark"></i>
         </div>
-        <img className='expanded-main-image' src={photos[photoId].url} alt='expanded main image'></img>
+        {zoom &&
+          <figure className='zoomed-main-image' style={{
+            backgroundImage: `url(${photos[photoId].url})`,
+            backgroundSize: `${zoomSize}`
+            }} onMouseMove={zoomIn} onClick={() => setZoom(false)}>
+            <img src={photos[photoId].url} alt='zoomed main image'></img>
+          </figure>
+        }
+        {!zoom &&
+          <img className='expanded-main-image' src={photos[photoId].url} onClick={handleZoomClick}></img>
+        }
         {!zoom &&
           <div className='expanded-thumbnail-view'>
             {photos.map((photo, i) => {
