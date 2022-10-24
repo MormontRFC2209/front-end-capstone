1c6ae6295168e73db0f43739a457114a106a0623
Sarah Handley <handleysarah99@gmail.com>
2022-10-24 16:57:01 -0500

    fix share button css

--- client/src/components/Overview/ShareMedia/ShareMedia.jsx
+++ client/src/components/Overview/ShareMedia/ShareMedia.jsx
@@ -6,21 +6,23 @@ export default function ShareMedia({ styles, selectedStyleId }) {
 
   return (
     <div>
-      <div className='share-product'> Share this product: </div>
+      <div className='share-product'> Share this product:
+        <div className='share-icon-container'>
+          <div data-href={window.location.origin}>
+            <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${siteLink}&amp;src=sdkpreparse`}>
+              <i className="fa-brands fa-square-facebook fa-2xl share-icon facebook"></i>
+            </a>
+          </div>
 
-      <div className="fb-share-button" data-href={window.location.origin}>
-        <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${siteLink}&amp;src=sdkpreparse`} className="fb-xfbml-parse-ignore">
-          <i className="fa-brands fa-square-facebook fa-2xl share-icon facebook"></i>
-        </a>
+          <a href={"https://twitter.com/intent/tweet?text=Check%20out%20" + siteLink}>
+            <i className="fa-brands fa-square-twitter fa-2xl share-icon twitter"></i>
+          </a>
+
+          <a href={"https://pinterest.com/pin/create/button/?url=" + siteLink + "&media=" + stylePhoto + "&description=Come%20visit%20our%20site!"}>
+            <i className="fa-brands fa-square-pinterest fa-2xl share-icon pinterest"></i>
+          </a>
+        </div>
       </div>
-
-      <a href={"https://twitter.com/intent/tweet?text=Check%20out%20" + siteLink}>
-        <i className="fa-brands fa-square-twitter fa-2xl share-icon twitter"></i>
-      </a>
-
-      <a href={"https://pinterest.com/pin/create/button/?url=" + siteLink + "&media=" + stylePhoto + "&description=Come%20visit%20our%20site!"}>
-        <i className="fa-brands fa-square-pinterest fa-2xl share-icon pinterest"></i>
-      </a>
     </div>
   );
 };
