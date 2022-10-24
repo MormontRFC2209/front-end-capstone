a85df21e8d5a682d520cb945da54d8f6d7afe3c7
Sarah Handley <handleysarah99@gmail.com>
2022-10-24 16:41:12 -0500

    fix facebook link

--- client/src/components/Overview/ShareMedia/ShareMedia.jsx
+++ client/src/components/Overview/ShareMedia/ShareMedia.jsx
@@ -1,16 +1,18 @@
 import react from 'react';
 
 export default function ShareMedia({ styles, selectedStyleId }) {
-  let siteLink = window.location.origin.split(':').join('%3A').split('/').join('%2F');
+  let siteLink = window.location.origin.split(':').join('%3A').split('/').join('%2F').split('localhost').join('127.0.0.1');
   let stylePhoto = styles[selectedStyleId].photos[0].url ? styles[selectedStyleId].photos[0].url.split(':').join('%3A') : 'Default.jpg';
 
   return (
     <div>
       <div className='share-product'> Share this product: </div>
 
-      <a target="_blank" href={"https://www.facebook.com/"}>
-        <i className="fa-brands fa-square-facebook fa-2xl share-icon facebook"></i>
-      </a>
+      <div className="fb-share-button" data-href={window.location.origin}>
+        <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${siteLink}&amp;src=sdkpreparse`} className="fb-xfbml-parse-ignore">
+          <i className="fa-brands fa-square-facebook fa-2xl share-icon facebook"></i>
+        </a>
+      </div>
 
       <a href={"https://twitter.com/intent/tweet?text=Check%20out%20" + siteLink}>
         <i className="fa-brands fa-square-twitter fa-2xl share-icon twitter"></i>
