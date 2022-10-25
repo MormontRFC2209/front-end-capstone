e2906bff9fe08deb98cf7796a68b2e2fb86ab302
Sarah Handley <handleysarah99@gmail.com>
2022-10-25 12:01:06 -0500

    rename tags classname and id for click tracking - share media and style selector

--- client/src/components/Overview/StyleSelector/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector/StyleSelector.jsx
@@ -1,5 +1,5 @@
-import react from 'react';
-import ThumbnailRow from './ThumbnailRow.jsx';
+import react from "react";
+import ThumbnailRow from "./ThumbnailRow.jsx";
 
 export default function StyleSelector({ styles, selectedStyleId, selectedStylePosition, clickStyle }) {
   let rowsOfThumbnails = Math.ceil(styles.length/4);
@@ -8,9 +8,10 @@ export default function StyleSelector({ styles, selectedStyleId, selectedStylePo
     let stylesOfRow = styles.slice((j * 4), ((j + 1) * 4));
     rows.push(stylesOfRow);
   }
+
   return (
-    <div className='styleSelectorRowsContainer'>
-      <div className='style-name'> <b> Style > </b> {styles[selectedStyleId].name} </div>
+    <div id="style-selector-rows-container" className="overview">
+      <div id="style-name" className="overview"> <b> Style > </b> {styles[selectedStyleId].name} </div>
       {rows.map((row, i) => {
         return (<ThumbnailRow key={i} rowKey={i} row={row} selectedStylePosition={selectedStylePosition} clickStyle={clickStyle}/>)
       })}
