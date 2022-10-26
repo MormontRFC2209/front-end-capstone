c090c1f52d91b4771005470f0242768683616dd6
Caleb <calebdunderwood@gmail.com>
2022-10-26 15:33:26 -0500

    Added styling to modals

--- client/src/components/Q&A/Modal/AnswerModal.jsx
+++ client/src/components/Q&A/Modal/AnswerModal.jsx
@@ -20,6 +20,10 @@ const initialValues = {
 
 var submittedImages = [];
 
+let checkQuestion = false;
+let checkUsername = false;
+let checkEmail = false;
+
 
 const Modal = ({isShowing, hide, id}) => {
 
@@ -34,8 +38,47 @@ const Modal = ({isShowing, hide, id}) => {
   const [currentInput, setInput] = useState(initialValues)
   const [imageInput, setImageInput] = useState(submittedImages)
 
-  var handleInputChange = (event) => {
-    const {name, value} = event.target;
+  var handleInputChange = (e) => {
+    const {name, value} = e.target;
+
+    if(name === 'answerform') {
+      if(value.length === 0){
+        e.target.setAttribute('aria-valid','inValid')
+        checkQuestion = false;
+      }
+      if(value.length >= 1){
+        e.target.setAttribute('aria-valid','valid')
+        checkQuestion = true;
+      }
+      if(value.length >= 1000){
+        e.target.setAttribute('aria-valid','inValid')
+        checkQuestion = false;
+      }
+    }
+    if(name === 'username') {
+      if(e.target.value.length === 0){
+        e.target.setAttribute('aria-valid','inValid')
+        checkUsername = false;
+      }
+      if(e.target.value.length >= 1){
+        e.target.setAttribute('aria-valid','valid')
+        checkUsername = true;
+
+      }
+      if(e.target.value.length >= 60){
+        e.target.setAttribute('aria-valid','inValid')
+        checkUsername = false;
+      }
+    }
+    if(name === 'email') {
+      if(!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value))) {
+        e.target.setAttribute('aria-valid','inValid')
+        checkEmail = false;
+      }else{
+        e.target.setAttribute('aria-valid','valid')
+        checkEmail = true;
+      }
+    }
 
     setInput({
       ...currentInput,
@@ -56,6 +99,15 @@ const Modal = ({isShowing, hide, id}) => {
 
   var submitAnswer = () => {
     event.preventDefault();
+
+    if(!(checkQuestion&&checkUsername&&checkEmail)){
+      if(!checkEmail) {
+        alert('Please provide an email address in the correct format.')
+        return;
+      }
+      alert('Please make sure that all inputs are valid.')
+      return;
+    }
 
 
     var answerObject = {
@@ -130,46 +182,81 @@ const Modal = ({isShowing, hide, id}) => {
             </button>
           </div>
           <form>
-            <label>Required
-              <textarea
-              type="text"
-              name="answerform"
-              placeholder="Place your answer here..."
-              required=""
-              autoComplete="off"
-              value={currentInput.name}
-              onChange={handleInputChange}
+          <div id="ANSWERDIV">
+                <div>
+                 <span>Answer</span><span>*</span>
+                </div>
+                <textarea
+                type="text"
+                name="answerform"
+                className="QANDA"
+                id="BODYOFINPUT"
+                rows="5"
+                cols="40"
+                placeholder="Why did you like the product or not?"
+                autoComplete="off"
+                value={currentInput.name}
+                onChange={handleInputChange}
 
-              ></textarea>
-            </label>
-            <label>What is your username?
-              <textarea
-              type="text"
-              name="username"
-              placeholder="Example: jack543!"
-              required=""
-              autoComplete="off"
-              value={currentInput.name}
-              onChange={handleInputChange}
-              ></textarea>
-            For privacy reasons, do not use your full name or email address.</label>
-            <label>Required
-              <textarea
-              type="text"
-              name="email"
-              placeholder="Example: jack@email.com"
-              required=""
-              autoComplete="off"
-              value={currentInput.name}
-              onChange={handleInputChange}
-              ></textarea>
-            For authentication reasons, you will not be emailed.</label>
+                ></textarea>
+            </div>
 
-            {imageInput.length < 5 ? <input type="file" name="answerImageURL" onChange={submitImage} /> : null}
-            {imageInput.length > 0 ? imageInput.map((singleImage) => {
-              return <img className="answerThumbnail" src={singleImage} key={Math.random()}></img>
-            }): null}
+            <div id="USERNAMEDIV">
+                <div>
+                  <span>Username</span><span>*</span>
+                </div>
+                <textarea
+                type="text"
+                name="username"
+                className="QANDA"
+                id="USERNAMEOFINPUT"
+                placeholder="Example: jackson11!"
+                autoComplete="off"
+                value={currentInput.name}
+                onChange={handleInputChange}
 
+                ></textarea>
+                <div>
+                  <span>For privacy reasons, do not use your full name or email address</span>
+                </div>
+            </div>
+
+            <div id="EMAILDIV">
+                <div>
+                  <span>Email</span><span>*</span>
+                </div>
+                <textarea
+                type="text"
+                name="email"
+                className="QANDA"
+                id="EMAILOFINPUT"
+                placeholder="Example: jack@email.com"
+                autoComplete="off"
+                value={currentInput.name}
+                onChange={handleInputChange}
+
+                ></textarea>
+                <div>
+                  <span>For authentication reasons, you will not be emailed</span>
+                </div>
+            </div>
+
+            {imageInput.length < 5 ?
+            <div id="ChooseAFile">
+              Upload A Photo!
+              <input type="file" name="answerImageURL" id="hiddenFileInput" onChange={submitImage} />
+            </div>
+            : null}
+
+            <div id="PHOTODIV">
+              {imageInput.length > 0 ? imageInput.map((singleImage) => {
+                return <img id="answerThumbnail" src={singleImage} key={Math.random()}></img>
+              }): null}
+            </div>
+
+            <div id="REQUIREDIV">
+              <span>* = Required</span>
+            </div>
             <button id="submitButton" onClick={submitAnswer}>Submit Answer.</button>
           </form>
         </div>
